5d9dfef8304737be9abeff0c054aa4354b32fd77
Jc <798025823@qq.com>
2019-12-23 17:56:33 +0800

    移植H743Pro版SD卡驱动,修复SD卡文件系统4字节对齐访问问题

--- freeRTOS+emXGUI综合程序_5寸_7寸/User/FATFS/drivers/fatfs_sd_sdio.c
+++ freeRTOS+emXGUI综合程序_5寸_7寸/User/FATFS/drivers/fatfs_sd_sdio.c
@@ -66,27 +66,50 @@ DSTATUS SD_status(BYTE lun){
     }
     return Stat;
 }
+
+DRESULT SD_ReadBlocks(BYTE *buff,//数据缓存区 
+                      DWORD sector, //扇区首地址
+                      UINT count)//扇区个数(1..128)
+{
+  DRESULT res = RES_ERROR;  
+  uint32_t alignedAddr;
+  RX_Flag = 0;
+  taskENTER_CRITICAL();
+  if(HAL_SD_ReadBlocks_DMA(&uSdHandle, (uint8_t*)buff,
+                           (uint32_t) (sector),
+                           count) == HAL_OK)
+  {
+    taskEXIT_CRITICAL();
+
+    GUI_SemWait(sem_sd,2000);
+
+          res = RES_OK;
+          alignedAddr = (uint32_t)buff & ~0x1F;
+          SCB_InvalidateDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
+  }
+  return res;  
+}
+
 DRESULT SD_read(BYTE lun,//物理扇区，多个设备时用到(0...)
                 BYTE *buff,//数据缓存区 
                 DWORD sector, //扇区首地址
                 UINT count)//扇区个数(1..128)
 {
   DRESULT res = RES_ERROR;
-  uint32_t timeout;
-  uint32_t alignedAddr;
-  RX_Flag = 0;
-
-  alignedAddr = (uint32_t)buff & ~0x1F;
-	/* 检查指针地址是否对齐,不对齐进行操作 */
+  uint32_t i;
+  DWORD pbuff[512/4];	
+  	
+	RX_Flag = 0;
+	
   if((DWORD)buff&3)
   {
-		DRESULT res = RES_OK;
+    DRESULT res = RES_OK;
     DWORD scratch[BLOCKSIZE / 4];
 
     while (count--) 
     {
       res = disk_read(0,(void *)scratch, sector++, 1);
-			SCB_InvalidateDCache_by_Addr((uint32_t*)scratch, BLOCKSIZE);//DMA读完数据清理Cache
+
       if (res != RES_OK) 
       {
         break;
@@ -95,52 +118,20 @@ DRESULT SD_read(BYTE lun,//物理扇区，多个设备时用到(0...)
       buff += BLOCKSIZE;
     }
     return res;
-	}
-  //更新相应的DCache(WB模式选用)
-//  SCB_CleanDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
-	GUI_MutexLock(mutex_lock,0xffffff);
-	/* 指针地址对齐,正常进行数据读取操作 */
-  if(HAL_SD_ReadBlocks_DMA(&uSdHandle, (uint8_t*)buff,
-                           (uint32_t) (sector),
-                           count) == HAL_OK)
-  {
-    /* Wait that the reading process is completed or a timeout occurs */
-    timeout = HAL_GetTick();
-    while((RX_Flag == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
-    {
-    }
-    /* incase of a timeout return error */
-    if (RX_Flag == 0)
-    {
-      res = RES_ERROR;
-    }
-    else
-    {
-      RX_Flag = 0;
-      timeout = HAL_GetTick();
-
-      while((HAL_GetTick() - timeout) < SD_TIMEOUT)
-      {
-        if (HAL_SD_GetCardState(&uSdHandle) == HAL_SD_CARD_TRANSFER)
-        {
-          res = RES_OK;
-
-          /*
-             the SCB_InvalidateDCache_by_Addr() requires a 32-Byte aligned address,
-             adjust the address and the D-Cache size to invalidate accordingly.
-           */
-          alignedAddr = (uint32_t)buff & ~0x1F;
-          //使相应的DCache无效
-          SCB_InvalidateDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
-
-           break;
-        }
-      }
-    }
   }
-	GUI_MutexUnlock(mutex_lock);
+  
+    GUI_MutexLock(mutex_lock,0xffffff);
+	 	for(i=0;i<count;i++)
+		{
+		 	res = SD_ReadBlocks((BYTE *)pbuff,sector+i,1);//单个sector的读操作
+      taskENTER_CRITICAL();
+			memcpy(buff,pbuff,512);
+      taskEXIT_CRITICAL();
+			buff+=512;
+		} 
+	
+  GUI_MutexUnlock(mutex_lock);
   return res;
-
 }
   
 DRESULT SD_write(BYTE lun,//物理扇区，多个设备时用到(0...)
@@ -148,49 +139,6 @@ DRESULT SD_write(BYTE lun,//物理扇区，多个设备时用到(0...)
                  DWORD sector, //扇区首地址
                  UINT count)//扇区个数(1..128)
 {
-#if 0
-    DRESULT res = RES_ERROR;
-    uint32_t timeout;
-    uint32_t alignedAddr;
-  
-    TX_Flag = 0;
-    alignedAddr = (uint32_t)buff & ~0x1F;
-    //更新相应的DCache
-    SCB_CleanDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
-    if(HAL_SD_WriteBlocks_DMA(&uSdHandle, (uint8_t*)buff,
-                             (uint32_t) (sector),
-                             count) == HAL_OK)
-    {
-      /* Wait that the reading process is completed or a timeout occurs */
-      timeout = HAL_GetTick();
-      while((TX_Flag == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
-      {
-      }
-      /* incase of a timeout return error */
-      if (TX_Flag == 0)
-      {
-        res = RES_ERROR;
-      }
-      else
-      {
-        TX_Flag = 0;
-        timeout = HAL_GetTick();
-
-        while((HAL_GetTick() - timeout) < SD_TIMEOUT)
-        {
-          if (HAL_SD_GetCardState(&uSdHandle) == HAL_SD_CARD_TRANSFER)
-          {
-            res = RES_OK;
-            //使相应的DCache无效
-            SCB_InvalidateDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
-
-             break;
-          }
-        }
-      }
-    }
-    return res;
-#endif
     DRESULT res = RES_ERROR;
     uint32_t timeout;
 
@@ -250,6 +198,7 @@ DRESULT SD_write(BYTE lun,//物理扇区，多个设备时用到(0...)
     }
     return res;
 }
+
 DRESULT SD_ioctl(BYTE lun,BYTE cmd, void *buff){
     DRESULT res = RES_ERROR;
     HAL_SD_CardInfoTypeDef CardInfo;
@@ -287,7 +236,7 @@ DRESULT SD_ioctl(BYTE lun,BYTE cmd, void *buff){
     default:
       res = RES_PARERR;
     }
-    return RES_OK;
+    return res;
 }
 
 //SDMMC1发送完成回调函数
